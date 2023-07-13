962c19bcf27971c20bf632b9cab46f69304585c1
Pseudomoaner <olijme2@hotmail.com>
2023-07-14 01:17:24 +0200

    Colormaps and rose plots
    Integrates colorBrewer colormaps (qualitative) and an initial version of the roseplot functionality into fibrePlotter

--- Plotting/plotLocalOrientationHistogram.m
+++ Plotting/plotLocalOrientationHistogram.m
@@ -1,4 +1,4 @@
-function [] = plotLocalOrientationHistogram(fibreProps)
+function [] = plotLocalOrientationHistogram(axes,inDat,cMap)
 %PLOTLOCALORIENTATIONHISTOGRAM plots the histogram of the local
 %orientations of fibres detected in the input AFM data.
 %
@@ -10,18 +10,17 @@ function [] = plotLocalOrientationHistogram(fibreProps)
 
 noBins = 18;
 
-angCollection = vertcat(fibreProps.localOrientation);
-
 angSet = linspace(0,pi,noBins+1);
-N = histcounts(angCollection+pi/2,'BinEdges',angSet);
-
-%Repeat N so the plot goes the full 360 degrees.
-N = [N';N';N(1)];
-
 angList = (angSet(1:end-1) + diff(angSet(1:2))/2)';
 angList = [angList;angList + pi;angList(1)];
 
-polarplot(angList,N,'r','lineWidth',1.5)
+for i = 1:size(inDat,1)
+    N = histcounts(inDat{i}+pi/2,'BinEdges',angSet);
 
-ax = gca;
-ax.ThetaLim = [0,180];
+    %Repeat N so the plot goes the full 360 degrees.
+    N = [N';N';N(1)];
+    
+    polarplot(axes,angList,N,'Color',cMap(i,:),'lineWidth',1.5)
+end
+
+axes.ThetaLim = [0,180];
